a652af0751dbe505b415c6b19c4f4ec95f5d988a
nagy filip <nagyfilip363@gmail.com>
2024-05-17 15:03:21 +0200

    basic UI

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-const test = document.getElementById("test");
-let line = 1;
-let len = 0;
-let end = false;
-document.addEventListener("keydown", (event) => {
-    const key = event.key.toUpperCase();
-    if (/^[A-Z]+$/.test(key) && !end) {
-        switch (key) {
-            case "BACKSPACE":
-                test.innerHTML = test.innerHTML.slice(0, -1);
-                len--;
-                break;
-            case "ENTER":
-                if (len >= 5) {
-                    console.log("aaaa");
-                    if (line >= 5) {
-                        end = true;
-                    }
-                    test.innerHTML += "<br>";
-                    line++;
-                    len = 0;
-                }
-                break;
-            default:
-                if (len < 5) {
-                    test.innerHTML += key;
-                    len++;
-                }
-                break;
-        }
-    };
-});
+function settingsWindow() {
+    const window = document.getElementById("window");
+    const windowWrapper = document.getElementById("window-wrapper");
+    const cs = getComputedStyle(document.querySelector(":root"));
+    windowWrapper.style.backgroundColor = "#550055";
+    console.log("tvoje máma");
+};
+
+document.getElementById("settings").addEventListener("click", settingsWindow)
