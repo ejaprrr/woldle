a4af53a824d2c7099434ff2604ea5c2d048f52cf
worxchi <worxchi@gmail.com>
2024-06-06 15:36:41 +0200

    update

--- js/info.js
+++ js/info.js
@@ -1,7 +1,7 @@
 // onclick events
 document.querySelector("#home").onclick = () => location.href = "index.html";
 
-const images = ['skola1.jpg', 'budova.jpg', 'infis.jpg'];
+const images = ['invite.png', 'gameplay.png', 'victory.png'];
 let currentIndex = 0;
 
 function rotateImage() {
@@ -10,8 +10,8 @@ function rotateImage() {
     imageElement.style.opacity = 0;
 
     setTimeout(() => {
-        imageElement.src = "images/" + images[currentIndex];
+        imageElement.src = "images/slideshow/" + images[currentIndex];
         imageElement.style.opacity = 1;
-    }, 650);
+    }, 250);
 }
 setInterval(rotateImage, 4500); // Change image every 3 seconds
